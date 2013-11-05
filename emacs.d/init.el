511bf7aaa2571de318a5610430a99b5e0c273664
mpkocher <michael.kocher@me.com>
2013-11-04 20:32:30 -0800

    Update init.el to better ruby setup.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -63,7 +63,7 @@
     (package-refresh-contents))
 
 (defvar prelude-packages
-  '(sr-speedbar org ace-jump-mode nrepl clojure-mode magit pylint scala-mode2 haskell-mode markdown-mode yasnippet graphviz-dot-mode fsharp-mode highlight-escape-sequences exec-path-from-shell)
+  '(geiser paredit edit-server sr-speedbar org ace-jump-mode nrepl clojure-mode magit pylint scala-mode2 haskell-mode markdown-mode yasnippet graphviz-dot-mode fsharp-mode highlight-escape-sequences exec-path-from-shell)
       "A list of packages to ensure are installed at launch.")
 
 (dolist (p prelude-packages)
@@ -89,6 +89,24 @@
       org-odd-levels-only t
       org-agenda-include-diary t)
 
+
+; elisp
+(require 'paredit)
+(require 'highlight-parentheses)
+
+;(setq hl-paren-colors '("red1" "orange1" "yellow1" "green1" "cyan1" "slateblue1" "magenta1" "purple"))
+
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+
+(require 'eldoc) ; if not already loaded
+(eldoc-add-command 'paredit-backward-delete 'paredit-close-round)
+
+; scheme
+(setq geiser-impl-installed-implementations '(racket))
+(require 'geiser)
+
+(setq geiser-racket-binary "/Applications/Racket v5.3.6/bin/racket")
+
 ; speedbar
 (require 'sr-speedbar)
 
@@ -105,9 +123,12 @@
 (require 'ess)
 ; set R exe
 
-
 ; auto complete
 (setq ess-use-auto-complete 'script-only)
+
+; 
+(require 'edit-server)
+;(edit-server-start)
 
 ; AceJump goodness
 (require 'ace-jump-mode)
@@ -117,7 +138,7 @@
 (require 'undo-tree)
 (global-undo-tree-mode 1)
 
-; Got place in the file when you reopen the buffer
+; Go to the place in the file when you reopen the buffer
 (require 'saveplace)
 (setq-default save-place t)
 
@@ -129,11 +150,15 @@
 
 (require 'magit)
 
-; Load pylint
-;(autoload 'python-pylint "python-pylint")
-;(autoload 'pylint "python-pylint")
+(require 'slime)
+(slime-setup)
 
-(add-hook 'clojure-mode-hook 'paredit-mode)
+; global clojure
+(require 'clojure-mode)
+
+(add-hook 'clojure-mode-hook (lambda () (highlight-parentheses-mode t) (paredit-mode t)
+(slime-mode t)))
+
 
 ; F# autocomplete
 (add-hook 'fsharp-mode-hook
@@ -150,7 +175,6 @@
 
 ; Slime mode
 (setq inferior-lisp-program "sbcl")
-(require 'slime)
 
 ;; Javascript
 (require 'js2-mode)
@@ -167,19 +191,18 @@
 ; Scala Support
 (require 'scala-mode2)
 
-; Python 
-(when (require 'elpy nil t)
-  (elpy-enable)
-  ;; Use the system python virtualenv with emacs.
-  ;(virtualenv-workon "dev_pbcore")
-  (virtualenv-workon "emacs")
-  ;; cleaner modeline
-  ;;(elpy-clean-modeline)
-  (setq elpy-rpc-backend "jedi")
-  (elpy-use-ipython)
-  ;; use iPython
-  (setq py-shell-name "ipython")
-)
+;; ; Python 
+;; (when (require 'elpy nil t)
+;;  (elpy-enable)
+;; ;  Use the system python virtualenv with emacs.
+;;  (virtualenv-workon "dev_pbcore")
+;; ;  cleaner modeline
+;;   (elpy-clean-modeline)
+;;  (setq elpy-rpc-backend "jedi")
+;;  (elpy-use-ipython)
+;; ;  use iPython
+;;  (setq py-shell-name "ipython")
+;; )
 
 ; IPython Notebook via eic
 ;(require 'eic)
@@ -190,20 +213,62 @@
 (setq company-minimum-prefix-length 2)
 
 ; Ruby 
-(require 'ruby-mode)
-(require 'rvm)
+;(require 'ruby-mode)
+;(require 'rvm)
+(require 'cl)
+(require 'compile)
 
-(rvm-use-default)
-(require 'inf-ruby)
+(require 'auto-complete-config)
+
+;(rvm-use-default)
+(setq enh-ruby-program "/Users/mkocher/.rvm/rubies/ruby-1.9.3-p194/bin/ruby")
+(require 'enh-ruby-mode)
+
+;; gem install pry pry-doc yard
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+
+(setq enh-ruby-bounce-deep-indent t)
+(setq enh-ruby-hanging-brace-indent-level 2)
+
+(add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'ruby-mode-hook 'yard-mode)
+
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'enh-ruby-mode-hook 'yard-mode)
+
+(push 'company-robe company-backends)
+(push 'ac-source-robe ac-sources)
+
+(ac-config-default)
+(setq ac-ignore-case nil)
+(add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
+
+(require 'smartparens-config)
+ (require 'smartparens-ruby)
+ (smartparens-global-mode)
+ (show-smartparens-global-mode t)
+ (sp-with-modes '(rhtml-mode)
+   (sp-local-pair "<" ">")
+   (sp-local-pair "<%" "%>"))
+
+;(require 'inf-ruby)
 ; http://www.emacswiki.org/emacs/InfRubyCompany
-(add-hook 'inf-ruby-mode-hook (lambda () (require 'inf-ruby-company)))
+;(add-hook 'inf-ruby-mode-hook (lambda () (require 'inf-ruby-company)))
 
-(defun my-inf-ruby-mode-hook ()
-    (set (make-local-variable 'ac-auto-start) 2)
-    (set (make-local-variable 'ac-auto-show-menu) t)
-)
+;; (defun my-inf-ruby-mode-hook ()
+;;     (set (make-local-variable 'ac-auto-start) 2)
+;;     (set (make-local-variable 'ac-auto-show-menu) t)
+;; )
 
-(add-hook 'inf-ruby-mode-hook 'my-inf-ruby-mode-hook)
+;(add-hook 'inf-ruby-mode-hook 'my-inf-ruby-mode-hook)
 
 ;; ido
 (require 'ido)
@@ -214,22 +279,32 @@
 (require 'osx-browse)
 (osx-browse-mode 1)
 
+; http://emacsfodder.github.io/blog/powerline-enhanced/
+; OSX with --cocoa doesn't work with this
+(require 'powerline)
+
+(powerline-default-theme)
+
 ;; Themes
 ;; (load-theme 'fogus)
-;; (load-theme 'jadedragon)
+;(load-theme 'jadedragon)
 (load-theme 'hickey)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8eaa3bce3c618cd81a318fcf2d28c1cd21278531f028feb53186f6387547dfb4" default))))
+ '(custom-safe-themes (quote ("73abbe794b6467bbf6a9f04867da0befa604a072b38012039e8c1ba730e5f7a5" "3b7e62b9884f1533f8eac5d21b252e5b8098274d7d9096521db84e4f10797ae3" "d0ff5ea54497471567ed15eb7279c37aef3465713fb97a50d46d95fe11ab4739" "dc46381844ec8fcf9607a319aa6b442244d8c7a734a2625dac6a1f63e34bc4a6" "f220c05492910a305f5d26414ad82bf25a321c35aa05b1565be12f253579dec6" "d293542c9d4be8a9e9ec8afd6938c7304ac3d0d39110344908706614ed5861c9" "8eaa3bce3c618cd81a318fcf2d28c1cd21278531f028feb53186f6387547dfb4" default)))
+ '(menu-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
 
 ;; Auto-refresh dired on file change
 (add-hook 'dired-mode-hook 'auto-revert-mode)
@@ -247,7 +322,3 @@
 
 ;; My location for external packages.
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
-
-; http://emacsfodder.github.io/blog/powerline-enhanced/
-; OSX with --cocoa doesn't work with this
-(require 'powerline)
